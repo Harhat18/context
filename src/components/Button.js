80328e5f9fe194f626953f7b1ae918d4e269b103
Harun HATİBOĞLU <111196660+Harhat18@users.noreply.github.com>
2022-10-21 22:59:50 +0300

    add

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,8 +2,7 @@ import { useContext } from "react";
 import ThemeContext from "../context/ThemeContext";
 function Button() {
   const data = useContext(ThemeContext);
-  console.log(data);
-  return <div>Button</div>;
+  return <div>Button {data} </div>;
 }
 
 export default Button;
