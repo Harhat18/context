add13e821d4706f5eaf035c3274a598ca281cb8f
Harun HATİBOĞLU <111196660+Harhat18@users.noreply.github.com>
2022-10-22 21:45:16 +0300

    add

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,4 +1,16 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 const ThemeContext = createContext();
 export default ThemeContext;
+// eslint-disable-next-line react-hooks/rules-of-hooks
+const [theme, setTheme] = useState("dark");
+const values = {
+  theme,
+  setTheme,
+};
+
+export const ThemeProvider = ({ children }) => {
+  return (
+    <ThemeContext.Provider value={values}>{children}</ThemeContext.Provider>
+  );
+};
