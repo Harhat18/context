ad7cece55e5f0dffb198d3f8328cab5ec3644e83
Harun HATİBOĞLU <111196660+Harhat18@users.noreply.github.com>
2022-10-23 09:07:14 +0300

    add

--- public/src/App.js
+++ public/src/App.js
@@ -5,13 +5,13 @@ import { ThemeProvider } from "./context/ThemeContext";
 import { UserProvider } from "./context/UserContext";
 
 function App() {
-	return (
-		<ThemeProvider>
-			<UserProvider>
-				<Container />
-			</UserProvider>
-		</ThemeProvider>
-	);
+  return (
+    <ThemeProvider>
+      <UserProvider>
+        <Container />
+      </UserProvider>
+    </ThemeProvider>
+  );
 }
 
 export default App;
