26c87f087faf37e2451ab7a8d7371d7b79063360
Harun HATİBOĞLU <111196660+Harhat18@users.noreply.github.com>
2022-10-23 08:02:48 +0300

    add

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -3,7 +3,7 @@ import { createContext, useState, useEffect } from "react";
 const ThemeContext = createContext();
 export default ThemeContext;
 // eslint-disable-next-line react-hooks/rules-of-hooks
-const [theme, setTheme] = useState(localStorage.setItem("theme"));
+const [theme, setTheme] = useState(localStorage.setItem("theme") || "light");
 // eslint-disable-next-line react-hooks/rules-of-hooks
 useEffect(() => {
   localStorage.setItem("theme", theme);
